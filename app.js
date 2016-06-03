e75da4c48ae9b487e3e53711351ebb2df8fcae0e
Kevin Katz <kevcmk@gmail.com>
2016-06-03 15:20:57 -0700

    Tweets streaming

--- app.js
+++ app.js
@@ -5,10 +5,38 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var fs = require('fs');
+var twitter = require('twitter');
+var util = require('util');
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+const project_directory = path.dirname(path.dirname(require.main.filename)); // Two directories above ./bin/www
+
+
 var app = express();
+
+
+const appConfigPath = path.join(project_directory, '.env.json');
+var appConfigContent = fs.readFileSync(appConfigPath, 'utf8');
+var appConfig = JSON.parse(appConfigContent);
+var tweeter = new twitter(appConfig);
+app.set('tweeter', tweeter);
+
+
+// Database setup
+var db;
+const MongoClient = require('mongodb').MongoClient
+
+MongoClient.connect(process.env.TRUMPIO_MONGO_DB_URL, (err, database) => {
+    if (err) {
+        throw new Error('Database failed to connect: ' + process.env.TRUMPIO_MONGO_DB_URL);
+    } else {
+        db = database;
+    }
+})
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,43 +46,28 @@ app.set('view engine', 'hbs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
+// app.use(errorHandler());
 app.use(cookieParser());
 app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+// Make our db accessible to our router
+app.use(function (req, res, next) {
+    req.db = db;
+    next();
+});
 
 app.use('/', routes);
 app.use('/users', users);
 
 
-
-/* Mongo Init */
-/*
-var MongoClient = require('mongodb').MongoClient
-    , assert = require('assert');
-
-// Connection URL
-var url = 'mongodb://localhost:27017/trumpio';
-// Use connect method to connect to the Server
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected correctly to server");
-
-  db.close();
-});
-*/
-
-// MongoClient
-var mongo = require('mongodb');
-var monk = require('monk');
-var db = monk('localhost:27017/nodetest1');
-
-
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -62,24 +75,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
-
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,24 @@ var router = express.Router();
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/*
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
+});
+*/
+
+router.get('/', function (req, res) {
+    var db = req.db;
+    // db.collection('tweets').save({tweet: "Hi" + (new Date()).toDateString()})
+
+    db.collection('tweets').find().toArray(function(err, results) {
+        console.log(results)
+        res.render('tweets', {
+            'title': "Trumpswing",
+            'tweets': results
+        });
+    })
+
 });
 
 module.exports = router;
